708e046438617e947ccb3f32710b67fc63a3ac15
Clement Poon <larrybob735@gmail.com>
2024-10-04 22:06:13 -0500

    COOL Spectrum Analyzer!!!

--- src/main.cpp
+++ src/main.cpp
@@ -27,14 +27,26 @@ float temp = 0;
 float hum = 0;
 
 //Ultrasonic sensor
-#define TRIGGER_PIN  4  // Arduino pin tied to trigger pin on the ultrasonic sensor.
-#define ECHO_PIN     5  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define TRIGGER_PIN  3  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     2  // Arduino pin tied to echo pin on the ultrasonic sensor.
 #define MAX_DISTANCE 500 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 float distance = 0;
 const int distrange = 20; //set distance range in inches
 const int diststart = 110; //set distance start in inches
-
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+
+//Spectral sensor
+#define STROBE 4
+#define RESET 5
+#define DC_One A0
+#define DC_Two A1
+//Define spectrum variables
+int freq_amp;
+int Frequencies_One[7][3];
+int Frequencies_Two[7][3];
+int isPeak_One[7];
+int isPeak_Two[7];
+int i;
 
 //Change I2c ports
 void PortChange(uint8_t bus) //function of TCA9548A
@@ -46,6 +58,16 @@ void PortChange(uint8_t bus) //function of TCA9548A
   
 }
 
+//Convert list to byte
+byte convertListToByte(int bits[]) {
+    byte result = 0;
+    for (int i = 0; i < sizeof(bits); ++i) {
+        if (bits[i] == 1) {
+            result |= (1 << i);
+        }
+    }
+    return result;
+}
 boolean getDistance() {
   //get distance from sensor
   distance = sonar.ping_cm(); // Send ping, get ping time in microseconds (uS).
@@ -54,8 +76,6 @@ boolean getDistance() {
   duration = duration/1000000; // Convert mircroseconds to seconds
   distance = (speedOfSound * duration)/2;
   distance = distance * 100; // meters to centimeters
-  Serial.print(F("Distance: "));
-  Serial.print(distance);
   /*set distance to 1-5
   if (distance <= diststart) {distance = 1;}
   else if (distance <= diststart+distrange) {distance = 2;}
@@ -78,10 +98,6 @@ boolean getTemp() {
   } else {
     temp = dht.readTemperature()*9/5+32;
     hum = dht.readHumidity();
-    Serial.print(F(" Temp: "));
-    Serial.print(temp);
-    Serial.print(F(" Humidity: "));
-    Serial.print(hum);
     return true;
   }
 }
@@ -222,6 +238,94 @@ void displayDistTemp() {
 
 }
 
+//Read spectral sensor
+void Read_Frequencies() {
+  digitalWrite(RESET, HIGH);
+  delayMicroseconds(50);
+  digitalWrite(RESET, LOW);
+  delayMicroseconds(50);
+
+  //Read frequencies for each band
+  for (freq_amp = 0; freq_amp < 7; freq_amp++)
+  {
+    digitalWrite(STROBE, HIGH);
+    delayMicroseconds(20);
+    digitalWrite(STROBE, LOW);
+    delayMicroseconds(20);
+    
+    //Read the first DC value
+    Frequencies_One[freq_amp][2] = Frequencies_One[freq_amp][1];
+    Frequencies_One[freq_amp][1] =  Frequencies_One[freq_amp][0];
+    Frequencies_One[freq_amp][0] = analogRead(DC_One);
+
+    //Read the second DC value
+    Frequencies_Two[freq_amp][2] = Frequencies_Two[freq_amp][1];
+    Frequencies_Two[freq_amp][1] =  Frequencies_Two[freq_amp][0];
+    Frequencies_Two[freq_amp][0] = analogRead(DC_Two);
+    if (Frequencies_One[freq_amp][0] < 90)
+    {
+      isPeak_One[freq_amp] = 0;
+    }
+    else
+    {
+      //Check for peaks (if the difference between the first and second DC values is greater than 10 and if the second and third DC values are less than the first and second DC values)
+      if ((Frequencies_One[freq_amp][0] - Frequencies_One[freq_amp][1] > 50) || (Frequencies_One[freq_amp][2]+Frequencies_One[freq_amp][1] < Frequencies_One[freq_amp][1]+Frequencies_One[freq_amp][0]))
+      {
+        //Display the peak
+        isPeak_One[freq_amp] = 1;
+      }
+      else
+      {
+        isPeak_One[freq_amp] = 0;
+      }
+    }
+    if (Frequencies_Two[freq_amp][0] < 90)
+    {
+      isPeak_Two[freq_amp] = 0;
+    }
+    else
+    {
+      if ((Frequencies_Two[freq_amp][0] - Frequencies_Two[freq_amp][1] > 50) || (Frequencies_Two[freq_amp][2]+Frequencies_Two[freq_amp][1] < Frequencies_Two[freq_amp][1]+Frequencies_Two[freq_amp][0]))
+      {
+        isPeak_Two[freq_amp] = 1;
+      }
+      else
+      {
+        isPeak_Two[freq_amp] = 0;
+      }
+    }
+  }
+}
+
+//display spectral sensor
+void displaySpectral() {
+  Read_Frequencies();
+  byte highByte = 0;
+  byte lowByte = 0;
+  for (int i = 0; i < 6; i++) { // Loop through the first 6 elements
+    if (isPeak_One[i] == 1) {
+      highByte |= (1 << 6-i); // Reverse position: set bit in lowByte instead of highByte
+    }
+    if (isPeak_Two[i] == 1) {
+      lowByte |= (1 << 6-i); // Reverse position: set bit in highByte instead of lowByte
+    }
+  }
+
+  // Handle the last element separately to light up 2 LEDs
+  if (isPeak_One[7] == 1) {
+    highByte |= (1 << 7); // Reverse position: set the 7th bit in lowByte
+  }
+  if (isPeak_Two[7] == 1) {
+    lowByte |= (1 << 7); // Reverse position: set the 8th bit in highByte
+  }
+
+  // Send the bytes to the shift registers
+  digitalWrite(latchPin, LOW);    // Start sending data
+  shiftOut(dataPin, clockPin, MSBFIRST, highByte);  // Send high byte first
+  shiftOut(dataPin, clockPin, MSBFIRST, lowByte);   // Send low byte second
+  digitalWrite(latchPin, HIGH);   // Latch the data to output
+  }
+
 void setup() {
   Wire.begin();
   Serial.begin(9600);
@@ -248,18 +352,28 @@ void setup() {
     for(;;);
   }
   display.display();
+
+  //Spectral sensor
+  //Set spectrum Shield pin configurations
+  pinMode(STROBE, OUTPUT);
+  pinMode(RESET, OUTPUT);
+  pinMode(DC_One, INPUT);
+  pinMode(DC_Two, INPUT);
+  //Initialize Spectrum Analyzers
+  digitalWrite(STROBE, LOW);
+  digitalWrite(RESET, LOW);
 }
 
 void loop() {
   //updates status of lights
-  int tempNum = updateTempLights();
+  //int tempNum = updateTempLights();
   //sends data to 74hc595
-  displayLevel(tempNum, tempNum);
+  //displayLevel(tempNum, tempNum);
+  displaySpectral();
   //display distance on OLED
-  displayDistTemp();
+  //displayDistTemp();
   //delay 
   
-  delay(1000);
   
 }
 
